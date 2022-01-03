f97d9df6bf01916e8df9757e191ce42b459d2c08
Şafak <40767892+SafakTavs@users.noreply.github.com>
2022-01-04 02:33:51 +0300

    deneme

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,36 @@
 function addToCart(productName = "Elma", quantity) {
-    console.log("Sepete Eklendi : " + productName)
+    console.log("Sepete Eklendi : " + productName  
+    + " Adet : " + quantity)
 }
 
-/* addToCart()
-addToCart("Yumurta")
-addToCart(15) */
+addToCart(10)
+
+
+
+let sayHello = () =>{
+    console.log("Hello World!")
+}
+
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello World2!")
+}
+
+sayHello2()
+
+function addToCart2(productName, quantity, unitPrice) {
+    
+}
+
+addToCart2("Elma",5,10)
+addToCart2("Armut",2,20)
+addToCart2("Limon",3,15)
+
+let product1 = {productName:"Elma", unitPrice:10 ,quantity:5}
+
+function addToCart3(product) {
+    console.log("Ürün : " + product.productName)
+}
+
+addToCart3(product1)
