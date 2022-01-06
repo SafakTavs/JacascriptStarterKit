71a24981bceb5b84f8aa7a3558d85a1c5b0f2c25
Şafak <40767892+SafakTavs@users.noreply.github.com>
2022-01-06 15:44:26 +0300

    deneme

--- src/functions.js
+++ src/functions.js
@@ -31,6 +31,29 @@ let product1 = {productName:"Elma", unitPrice:10 ,quantity:5}
 
 function addToCart3(product) {
     console.log("Ürün : " + product.productName)
+    console.log("Adet : " + product.quantity)
+    console.log("Fiyat : " + product.unitPrice)
 }
 
 addToCart3(product1)
+
+let product2 = {productName:"Elma", unitPrice:10 ,quantity:5}
+let product3 = {productName:"Elma", unitPrice:10 ,quantity:5}
+product2 = product3
+product2.productName = "Karpuz"
+console.log(product3.productName)
+
+
+let sayi1 = 10
+let sayi2 = 20
+sayi1 = sayi2
+sayi2 = 100
+console.log(sayi1)
+
+
+function addToCart4(products) {
+    console.log(products)
+}
+
+
+products = []
